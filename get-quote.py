94243d1d8dacd51e4259bd9f6d998f261612a229
Hamza-21 <69684098+Hamza-21@users.noreply.github.com>
2020-09-07 22:03:39 +0530

    Resolved some issues

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,16 @@
 import random
 
 def quote():
-  #print("Keep it logically awesome.")
-
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
-  rnd = random.randint(0,13)
-  print(quotes[rnd])
+
+  last = 18
+  rnd = random.randint(0,18)
+  msr = random.randint(0,18)
+  print(quotes[rnd], end="")
+  print(quotes[msr], end="")
+
 
 if __name__== "__main__":
   quote()
